1aa2ef31c25844c530ff8ee8fdfd3103093c2790
Unknown <darkviruz@gmail.com>
2018-07-05 23:39:03 +0200

    05/07/2018

---
+++ 1_esencial/11_ajax_axios.js
@@ -0,0 +1,21 @@
+new Vue({
+    el: 'main',
+    mounted() {
+        this.cargarPersonas();
+    },
+    data: {
+        personas: []
+    },
+    methods: {
+        cargarPersonas() {
+            // this.$http.get('https://randomuser.me/api/?results=10')
+            //     .then((respuesta) => {
+            //         this.personas = respuesta.body.results;
+            //     })
+            axios.get('https://randomuser.me/api/?results=10')
+                .then((respuesta) => {
+                    this.personas = respuesta.data.results;
+                })
+        }
+    }
+})
