ea42a28660418555b75025e9d85c0e12a0b4fb53
Unknown <darkviruz@gmail.com>
2018-07-12 00:30:22 +0200

    12/07/2018

---
+++ 2_componentes/03_component_properties.js
@@ -0,0 +1,22 @@
+Vue.component('autor', {
+    props: ['nombre','edad'],
+    mounted(){
+        // Las props son acesibles desde this (proxy)
+        // console.log(this.nombre);
+        console.log(typeof this.edad);
+    },
+    template: `<div><h1>{{nombre}}</h1>
+                <button @click="cambiarProp">Cambiar Prop</button></div>`,
+    methods: {
+        cambiarProp(){
+            this.nombre = this.nombre.toUpperCase();
+        }
+    }
+})
+
+new Vue({
+    el: 'main',
+    data: {
+        autor: 'Juan Andrés',
+    }
+})
